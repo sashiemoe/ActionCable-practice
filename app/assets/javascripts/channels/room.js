6e74328c34e7fd0f911564004c08ea39a1e90f7b
さしえもえ <sashie.moe@heartsoft.co.jp>
2020-10-14 13:30:00 +0900

    メッセージが送信できる

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,28 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function() {
+    // Called when there's incoming data on the websocket for this channel
+  },
+
+  speak: function(data) {
+      //  window.alert(data)
+      //return this.perform('speak');
+      return $('#messages').append(data['message']);
+  }
+});
+
+$(document).on('keypress', '[data-behavior~=room_speaker]',function (event) {
+    if (event.keyCode===13) {
+        console.log(event.target.value);
+        App.room.speak(event.target.value);
+        event.target.value='';
+        return event.preventDefault();
+    }
+})
